eb7c487ee92b351f00c0735f151472f9aa3257c1
Максим Захаров <JuzeHDHD@gmail.com>
2025-05-17 18:31:50 +0700

    map

--- src/components/sections/Place.jsx
+++ src/components/sections/Place.jsx
@@ -1,3 +1,5 @@
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+
 export default () => {
     return (
         <div className="section-place">
@@ -15,9 +17,25 @@ export default () => {
                     <img src="bogema.png"/>
                 </div>
                 <div className="section-place__info-text">
-                    <img src="bogema.png"/>
+                    <div>Manya Bar зал «Bogema» 2 этаж</div>
+                    <div>Адрес: просп. Строителей, 18, корп. 5</div>
                 </div>
             </div>
+
+            <ManyaMap/>
+
         </div>
     )
+}
+
+ function ManyaMap() {
+  return (
+    <div className='manya-map'>
+        <YMaps>
+            <Map height={'45vh'} width={'100%'} defaultState={{ center: [53.766196, 87.109144], zoom: 15 }}>
+                <Placemark geometry={[53.766196, 87.109144]} />
+            </Map>
+        </YMaps>
+    </div>
+  );
 }
