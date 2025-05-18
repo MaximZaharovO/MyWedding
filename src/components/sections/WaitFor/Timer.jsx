d106e64c3e0227e754120a7f2e773016114325e3
Максим Захаров <JuzeHDHD@gmail.com>
2025-05-18 16:18:09 +0700

    adaptive end

--- src/components/sections/WaitFor/Timer.jsx
+++ src/components/sections/WaitFor/Timer.jsx
@@ -15,24 +15,24 @@ const Timer = ({ end }) => {
     } else {
       // Render a countdown
       return <div style={{display: 'flex'}}>
-        <div style={{margin: '0 12px'}}>
+        <div style={{margin: '0 0.75rem'}}>
           <div style={{marginBottom: 8}}>{TwoNumber(days)}</div>
-          <div style={{fontSize: "24px"}}>Дней</div>
+          <div style={{fontSize: "1.5rem"}}>Дней</div>
         </div>
         <div>:</div>
-        <div style={{margin: '0 12px'}}>
+        <div style={{margin: '0 0.75rem'}}>
           <div style={{marginBottom: 8}}>{TwoNumber(hours)}</div>
-          <div style={{fontSize: "24px"}}>Часов</div>
+          <div style={{fontSize: "1.5rem"}}>Часов</div>
         </div>
         <div>:</div>
-        <div style={{margin: '0 12px'}}>
+        <div style={{margin: '0 0.75rem'}}>
           <div style={{marginBottom: 8}}>{TwoNumber(minutes)}</div>
-          <div style={{fontSize: "24px"}}>Минут</div>
+          <div style={{fontSize: "1.5rem"}}>Минут</div>
         </div>
         <div>:</div>
-        <div style={{margin: '0 12px'}}>
+        <div style={{margin: '0 0.75rem'}}>
           <div style={{marginBottom: 8}}>{TwoNumber(seconds)}</div>
-          <div style={{fontSize: "24px"}}>Секунд</div>
+          <div style={{fontSize: "1.5rem"}}>Секунд</div>
         </div>
       </div>;
     }
